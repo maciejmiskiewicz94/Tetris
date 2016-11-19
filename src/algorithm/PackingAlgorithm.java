4ab132eeff4cf6d83ec04ba21e68b24b5cb0ee8f
maciejmiskiewicz94 <maciej.miskiewicz94@gmail.com>
2016-11-19 14:51:02 +0100

    putting tiles in wells works

--- src/algorithm/PackingAlgorithm.java
+++ src/algorithm/PackingAlgorithm.java
@@ -48,6 +48,8 @@ public class PackingAlgorithm {
                                 currentHeight--;
                                 counter = 0;
                                 a = 0;
+                                i--;
+                                j--;
                             }
                         }
                     }
