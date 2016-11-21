9ab18707775fd290de61429710ff686bd73cbf64
maciejmiskiewicz94 <maciej.miskiewicz94@gmail.com>
2016-11-22 00:09:39 +0100

    deserializing added, not finished

--- src/helpers/Serialization.java
+++ src/helpers/Serialization.java
@@ -1,12 +1,16 @@
 package helpers;
 
+import controller.TilesManager;
+import data.ProcessingTile;
 import data.Tile;
 import data.Well;
+import gui.TilesGuiGenerator;
 import org.omg.CORBA.Environment;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringJoiner;
 
 /**
@@ -14,6 +18,9 @@ import java.util.StringJoiner;
  */
 public class Serialization {
     private int id;
+    private ArrayList<Well> wells1;
+    private ArrayList<ArrayList<ProcessingTile>> tiles1;
+    private int backtrack;
     public Serialization(int id)
     {
         this.id=id;
@@ -29,14 +36,14 @@ public class Serialization {
         {
             int a=wellList.get(i).getWidth();
             int b=wellList.get(i).getHeight();
-            output+=Integer.toString(a)+" "+Integer.toString(b)+System.lineSeparator();
+            output+=Integer.toString(a)+" "+Integer.toString(b)+" "+System.lineSeparator();
             for(int x=0;x<b;x++)
             {
                 for(int y=0;y<a;y++)
                 {
                     output+=Integer.toString(wellList.get(i).well[x][y])+ " ";
                 }
-                output+=System.lineSeparator();
+                output+=" "+System.lineSeparator();
             }
             ArrayList<Tile> currentTiles=tilesOfTilesList.get(i);
             output+=Integer.toString(currentTiles.size())+System.lineSeparator();
@@ -44,7 +51,7 @@ public class Serialization {
             {
                 int width=currentTiles.get(iter).getWidth();
                 int heighht=currentTiles.get(iter).getHeight();
-                output+=Integer.toString(width)+" "+Integer.toString(heighht)+System.lineSeparator();
+                output+=Integer.toString(width)+" "+Integer.toString(heighht)+" "+System.lineSeparator();
                 for(int tileheight=0;tileheight<heighht;tileheight++)
                 {
                     for(int tilewidth=0;tilewidth<width;tilewidth++)
@@ -69,4 +76,63 @@ public class Serialization {
         }
 
     }
+    public void deserialize (File file, TilesManager manager)
+    {
+        TilesGuiGenerator TGG=new TilesGuiGenerator(1);
+        int width=0;
+        try (Scanner sc = new Scanner(file)) {
+            this.backtrack = sc.nextInt();
+            sc.nextLine();
+            for(int i=0;i<backtrack;i++)
+            {
+                width=sc.nextInt();
+                wells1.add(i, new Well(width,TGG.generateWell(width,getMultiplier(width)), getMultiplier(width)));
+                wells1.get(i).setHeight(sc.nextInt());
+                sc.nextLine();
+                for(int h=0;h<wells1.get(i).getWidth();h++)
+                {
+                    for(int w=0;w<wells1.get(i).getWidth();w++)
+                    {
+                        wells1.get(i).well[h][w]=sc.nextInt();
+                    }
+                    sc.nextLine();
+                }
+                sc.nextLine();
+                int numTiles=sc.nextInt();
+                sc.nextLine();
+                for(int j=0;j<numTiles;j++)
+                {
+                   //TODO readind tiles
+
+
+                }
+            }
+
+        }
+        catch(FileNotFoundException ex)
+        {
+
+        }
+    }
+
+    private int getMultiplier(int width) {
+        int multi=0;
+        if((width>0)&&(width<=50)){
+          //  wellWSize = wellWidth*10;
+            multi=10;
+        }
+        else if((width>50)&&(width<100)){
+        //    wellWSize = wellWidth*5;
+            multi=5;
+        }
+        else{
+           // wellWSize = wellWidth;
+            multi=1;
+        }
+        return multi;
+    }
+
 }
+
+
+
